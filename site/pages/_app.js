42bf7061b21e1311d3914d5b0bdb8e2adb93ebf5
RenaudRDB <73218341+RenaudRDB@users.noreply.github.com>
2022-05-20 16:19:05 +0200

    Push du tableau de fausse données

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  
+  const codeblocks = [
+    {
+      title: "test1",
+      tag: ["Violent", "mot"],
+      code: "Je vais manger vos morts!"
+    },
+    {
+      title: "Vomit",
+      tag: ["cellule", "alcool"],
+      code: "Ceci est le code d'un code codé"
+    }
+    ];
+
   return <Component {...pageProps} />
 }
 
